18c6e50ae8a39faa9bf2be751e1a3742f4f080fd
Michael Dojčár <michaeldojcar@gmail.com>
2020-04-24 20:51:36 +0200

    assets refactor

--- Resources/views/components/add_modal.blade.php
+++ Resources/views/components/add_modal.blade.php
@@ -1,37 +1,57 @@
 <div class="card">
     <div class="card-header">Přiřazené fotografie
-        <a href="#" class="btn btn-success btn-sm float-right" data-toggle="modal" data-target="#exampleModal">Nahrát fotografii</a>
+        <a href="#"
+           class="btn btn-success btn-sm float-right"
+           data-toggle="modal"
+           data-target="#exampleModal">Nahrát fotografii</a>
     </div>
     <div class="card-body">
         <div class="row">
             @foreach($model->photos as $photo)
                 <div class="col-4">
                     <img class="img-thumbnail"
-                         src="{{$photo->url()}}">
+                         src="{{$photo->size(200, 150)}}">
                 </div>
             @endforeach
         </div>
     </div>
 </div>
 
-<form action="{{}}">
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+<form action="{{route($action, $model)}}" enctype="multipart/form-data" method="POST">
+    @csrf
+    <div class="modal fade"
+         id="exampleModal"
+         tabindex="-1"
+         role="dialog"
+         aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog"
+             role="document">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Nahrávání fotky</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Vyberte fotografii</label>
+                        <input type="file"
+                               name="photo" class="form-control-file">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button"
+                            class="btn btn-secondary"
+                            data-dismiss="modal">Storno
+                    </button>
+                    <input type="submit"
+                            class="btn btn-primary" value="Nahrát">
+                </div>
             </div>
         </div>
     </div>
-</div>
 </form>
